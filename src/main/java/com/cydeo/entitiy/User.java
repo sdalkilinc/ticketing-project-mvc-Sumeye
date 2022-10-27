7b0a62ed74d4d78796739f39b73bf90547945288
Sumeye <dalkilincsumeye@gmail.com>
2022-10-27 19:17:51 -0400

    More work added

--- src/main/java/com/cydeo/entitiy/User.java
+++ src/main/java/com/cydeo/entitiy/User.java
@@ -1,7 +1,13 @@
 package com.cydeo.entitiy;
 
 import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
 public class User extends BaseEntity{
 
     private String firstName;
@@ -13,4 +19,15 @@ public class User extends BaseEntity{
     private Role role;
     private Gender gender;
 
+    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String passWord, boolean enabled, String phone, Role role, Gender gender) {
+        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.passWord = passWord;
+        this.enabled = enabled;
+        this.phone = phone;
+        this.role = role;
+        this.gender = gender;
+    }
 }
