f82b2509888a4e69c77b950e9666136eaecbce12
Sumeye <dalkilincsumeye@gmail.com>
2022-10-28 14:43:00 -0400

    More work added

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -15,6 +17,8 @@ public class UserController {
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
+        List<Ro>
+        model.addAttribute("roles", new  )
 
         return "/user/create";
 
