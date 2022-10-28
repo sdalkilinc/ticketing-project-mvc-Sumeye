57a05a50f63d4fb7fce369c03006c8c92c4fb952
Sumeye <dalkilincsumeye@gmail.com>
2022-10-28 12:18:14 -0400

    More work added

---
+++ src/main/java/com/cydeo/dto/RoleDTO.java
@@ -0,0 +1,16 @@
+package com.cydeo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDTO {
+
+    //10.video 1.07 dakika
+    private Long id;
+    private String description;
+
+}
