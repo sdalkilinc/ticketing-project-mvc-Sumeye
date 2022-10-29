9802abbf6b8b71de4cb2c54c9337704c1d080f59
Sumeye <dalkilincsumeye@gmail.com>
2022-10-29 09:59:44 -0400

    More work added

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -65,4 +65,16 @@ public class UserController {
         return "redirect:/user/create";
     }
 
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+
+        userService.deleteById(username);
+
+        return "redirect:/user/create";
+    }
+
+
+
+
+
 }
