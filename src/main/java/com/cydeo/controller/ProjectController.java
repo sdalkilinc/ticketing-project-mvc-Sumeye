74104e3a23aef0c14c8f7d03ffb1f12559c22c7d
Sumeye <dalkilincsumeye@gmail.com>
2022-10-29 10:35:43 -0400

    More work added

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,8 +1,11 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectController {
 
     @RequestMapping("/create")
-    public String createProject(){
+    public String createProject(Model model){
+
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("managers", UserDTO user);
 
         return "/project/create";
     }
---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,7 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+
+public interface ProjectService extends CrudService<ProjectDTO, String>{
+    //Crud service is looking 2 things: 1)which object? 2)the unique thing
+}
