8da692a51055f4a9c8a0659edc386a5aa2290df0
Sumeye <dalkilincsumeye@gmail.com>
2022-10-29 10:59:32 -0400

    More work added

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -2,8 +2,8 @@ package com.cydeo.controller;
 
 
 import com.cydeo.dto.ProjectDTO;
-import com.cydeo.dto.UserDTO;
-import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/project")
 public class ProjectController {
 
+
+    private final UserService userService;
+    private final ProjectService projectService;
+
+    public ProjectController(UserService userService, ProjectService projectService) {
+        this.userService = userService;
+        this.projectService = projectService;
+    }
+
     @RequestMapping("/create")
     public String createProject(Model model){
 
         model.addAttribute("project", new ProjectDTO());
-        model.addAttribute("managers", UserDTO user);
+
+        model.addAttribute("managers", userService.findAll());
+
+        model.addAttribute("projects", projectService.findAll());
 
         return "/project/create";
     }
