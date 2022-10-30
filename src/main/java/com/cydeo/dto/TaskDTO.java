94eb1a403db10d2a592137b78189dd85a939a70c
Sumeye <dalkilincsumeye@gmail.com>
2022-10-30 15:41:14 -0400

    More work added

--- src/main/java/com/cydeo/dto/TaskDTO.java
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -12,12 +12,12 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class TaskDTO {
 
+    private Long id;
     private ProjectDTO project;
     private UserDTO assignedEmployee;
     private String taskSubject;
     private String taskDetail;
     private Status taskStatus;
     private LocalDate assignedDate;
-
 
 }
---
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -0,0 +1,8 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.TaskDTO;
+
+public interface TaskService extends CrudService<TaskDTO, Long>{
+
+
+}
