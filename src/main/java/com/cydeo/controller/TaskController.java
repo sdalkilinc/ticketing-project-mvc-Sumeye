c94832f042e56ece293313864a61e4b422a7e757
Sumeye <dalkilincsumeye@gmail.com>
2022-10-30 15:35:25 -0400

    More work added

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,20 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ProjectDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+    @RequestMapping("/create")
+    public String createTask(Model model){
+
+
+        return "task/create";
+    }
+
+
+}
