2cd398102e99dc716a376c72d8e2dacae445d3a5
Sumeye <dalkilincsumeye@gmail.com>
2022-10-31 09:43:14 -0400

    More work added

---
+++ src/main/java/com/cydeo/converter/ProjectDtoConverter.java
@@ -0,0 +1,22 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.ProjectService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
+
+    ProjectService projectService;
+
+    public ProjectDtoConverter(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @Override
+    public ProjectDTO convert(String source) {
+        return projectService.findById(source);
+    }
+}
--- src/main/java/com/cydeo/dto/TaskDTO.java
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -13,8 +13,10 @@ import java.time.LocalDate;
 public class TaskDTO {
 
     private Long id;
+
     private ProjectDTO project;
     private UserDTO assignedEmployee;
+
     private String taskSubject;
     private String taskDetail;
     private Status taskStatus;
