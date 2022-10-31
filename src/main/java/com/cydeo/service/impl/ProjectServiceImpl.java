5bec61c8d0f767411ba4c1bc3a32e4b5c75e4095
Sumeye <dalkilincsumeye@gmail.com>
2022-10-31 15:47:34 -0400

    More work added

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,10 +1,11 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
-import com.cydeo.service.CrudService;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,13 @@ import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements ProjectService{
+
+    private final TaskService taskService;
+
+    public ProjectServiceImpl(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @Override
     public ProjectDTO save(ProjectDTO project) {
 
@@ -59,6 +67,9 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> i
                        stream().
                        filter(project -> project.getAssignedManager().equals(manager))
                        .map(project ->{
+
+                           List<TaskDTO> taskList = taskService.findTasksByManager(manager);
+
                            int completeTaskCounts = 5;
                            int unfinishedTaskCounts = 3;
 
