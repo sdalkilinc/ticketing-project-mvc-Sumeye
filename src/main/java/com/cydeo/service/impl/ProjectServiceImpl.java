0238666e5261a635887c4c4767a792daab92b990
Sumeye <dalkilincsumeye@gmail.com>
2022-10-31 15:53:18 -0400

    More work added

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -70,8 +70,8 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> i
 
                            List<TaskDTO> taskList = taskService.findTasksByManager(manager);
 
-                           int completeTaskCounts = 5;
-                           int unfinishedTaskCounts = 3;
+                           int completeTaskCounts = (int) taskList.stream().filter(t ->t.getProject().equals(project) && t.getTaskStatus()==Status.COMPLETE).count();
+                           int unfinishedTaskCounts = (int) taskList.stream().filter(t ->t.getProject().equals(project) && t.getTaskStatus() != Status.COMPLETE).count();
 
                            project.setCompleteTaskCounts(completeTaskCounts);
                            project.setUnfinishedTaskCounts(unfinishedTaskCounts);
